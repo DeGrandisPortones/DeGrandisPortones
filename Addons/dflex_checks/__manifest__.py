4540a3c7c80f572a169e427ef7a241fc243ad490
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-07 16:31:21 -0300

    rompi

--- Addons/dflex_checks/__manifest__.py
+++ Addons/dflex_checks/__manifest__.py
@@ -1,18 +1,21 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'DFlex - Importador de Portones (Excel)',
-    'version': '1.0',
-    'category': 'Manufacturing',
-    'summary': 'Importa portones desde un archivo Excel y genera fichas automáticas',
-    'depends': ['base', 'mrp'],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/x_gate_spec_views.xml',
-        'views/x_gate_import_batch_views.xml',
-        'views/gate_import_wizard_views.xml',
-        'views/menuitems.xml',
+    "name": "DFlex - Importador de Portones (Excel)",
+    "summary": "Sube un Excel tal como está y crea fichas de portón con todos los valores. (Solo De Grandis Portones)",
+    "version": "18.0.1.2",
+    "author": "DFlex Argentina SAS",
+    "website": "https://dflex.com.ar",
+    "category": "Manufacturing",
+    "license": "LGPL-3",
+    "depends": ["mrp", "base"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/x_gate_spec_views.xml",
+        "views/x_gate_import_batch_views.xml",
+        "views/gate_import_wizard_views.xml",
+        "views/menuitems.xml"
     ],
-    'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
+    "installable": True,
+    "application": False,
 }
