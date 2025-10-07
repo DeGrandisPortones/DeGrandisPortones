9dac94b9da9ee86722535fbe5c6ffbde149bfc5f
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-07 15:25:15 -0300

    Lets go

--- Addons/dflex_gate_import/__manifest__.py
+++ Addons/dflex_gate_import/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "DFlex - Importador de Portones (Excel)",
     "summary": "Sube un Excel tal como está y crea fichas de portón con todos los valores. (Solo De Grandis Portones)",
-    "version": "18.0.1.1",
+    "version": "18.0.1.2",
     "author": "DFlex Argentina SAS",
     "website": "https://dflex.com",
     "category": "Manufacturing",
@@ -11,10 +11,10 @@
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
-        "views/menuitems.xml",
         "views/x_gate_spec_views.xml",
         "views/x_gate_import_batch_views.xml",
         "views/gate_import_wizard_views.xml",
+        "views/menuitems.xml"
     ],
     "installable": True,
     "application": False,
