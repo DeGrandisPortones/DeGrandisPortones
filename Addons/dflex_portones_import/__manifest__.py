2895e83e82e8f79d788dae20c05a0a9eba557920
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-08 12:35:16 -0300

    Change Manifest

--- Addons/dflex_portones_import/__manifest__.py
+++ Addons/dflex_portones_import/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'DFLEX Portones - Importador',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'summary': 'Importa portones desde Excel/XLS para tabular especificaciones.',
     'author': 'Esteban Scalerandi + ChatGPT',
     'website': 'https://dflex.com.ar',
@@ -9,12 +9,12 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'views/porton_menu.xml',
-        'views/porton_views.xml',
-        'views/porton_spec_views.xml',
+        'views/porton_views.xml',          # define action first
+        'views/porton_spec_views.xml',     # define related views
+        'views/porton_menu.xml',           # then reference actions in menus
         'wizard/import_wizard_views.xml',
     ],
-    'assets': {},  # No web assets yet
+    'assets': {},
     'installable': True,
     'application': False,
 }
