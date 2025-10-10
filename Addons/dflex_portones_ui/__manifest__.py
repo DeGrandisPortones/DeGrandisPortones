a721ffd65cd5228ba0c27592039dace0b5311f99
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-10 09:21:23 -0300

    Nuevo modulo

---
+++ Addons/dflex_portones_ui/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DFlex - Portones UI",
+    "version": "1.1.0",
+    "category": "Tools",
+    "summary": "Lanzador y vistas para x_dflex.porton",
+    "author": "Dflex Argentina SAS",
+    "license": "LGPL-3",
+    "depends": ["base", "dflex_portones_import"],
+    "data": [
+        "views/porton_views.xml",
+        "views/launcher_views.xml",
+        "views/menu.xml"
+    ],
+    "application": False,
+    "installable": True
+}
