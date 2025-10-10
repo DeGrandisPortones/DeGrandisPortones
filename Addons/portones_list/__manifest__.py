1ed7d6989fa48b8ef91946d1fb1a37b9b30504bc
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-10 11:52:32 -0300

    test

---
+++ Addons/portones_list/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Portones – Lista",
+    "version": "18.0.1.0",
+    "summary": "Vista de lista para x_dflex.porton",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",   
+        "views/x_dflex_porton_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "license": "LGPL-3",
+}
