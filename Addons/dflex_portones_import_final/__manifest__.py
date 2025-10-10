cf62572f8aeab98d6088cdb3f02abff4d0e2d5a4
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-10-10 11:27:08 -0300

    Final

---
+++ Addons/dflex_portones_import_final/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DFlex - Importador de Portones",
+    "version": "1.0.0",
+    "category": "Tools",
+    "summary": "Importa portones desde CSV al modelo x_dflex.porton",
+    "author": "Dflex Argentina SAS",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/wizard_views.xml",
+        "views/menu.xml"
+    ],
+    "application": False,
+    "installable": True
+}
