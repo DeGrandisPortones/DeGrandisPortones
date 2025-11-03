347191323364217ceae171fe9d943527c6f60667
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-03 15:01:51 -0300

    Payment good

---
+++ Addons/Payment Receipt - Cheques/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Payment Receipt - Cheques",
+    "summary": "Add check lines to customer payments and print them on the Payment Receipt",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_payment_views.xml",
+        "reports/report_payment_receipt_checks.xml"
+    ],
+    "license": "LGPL-3",
+    "installable": true,
+    "application": false,
+    "author": "Esteban + ChatGPT"
+}
