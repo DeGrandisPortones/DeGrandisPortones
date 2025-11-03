84736559d8af02429ec1aa293b3cf744d0cbe192
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-03 15:21:48 -0300

    abemus

--- Addons/Payment_receipt_cheques/__manifest__.py
+++ Addons/Payment_receipt_cheques/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Payment Receipt - Cheques",
     "summary": "Add check lines to customer payments and print them on the Payment Receipt",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.0.3",
     "category": "Accounting/Accounting",
     "depends": ["account"],
     "data": [
