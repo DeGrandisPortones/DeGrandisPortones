a2c10a081393b410c798561662b8ecbb0df8df48
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-04 16:04:22 -0300

    Dfelxs

--- Addons/dflex_payment_receipt/__manifest__.py
+++ Addons/dflex_payment_receipt/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'DFlex Payment Receipt (Modern)',
-    'summary': 'Recibo de pago moderno: transferencias, e-cheques, imputaciones y observaciones',
-    'version': '18.0.1.0.0',
+    'summary': 'Recibo de pago moderno: transferencias, e-cheques, imputaciones y observaciones (fixD)',
+    'version': '18.0.1.0.1',
     'category': 'Accounting/Reporting',
     'depends': ['account', 'web'],
     'data': ['reports/report_payment_receipt.xml'],
