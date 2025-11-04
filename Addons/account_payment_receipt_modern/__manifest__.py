f2dd09d0783f25e68df774f9535c2f956902fd25
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-04 15:20:12 -0300

    ver

---
+++ Addons/account_payment_receipt_modern/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Account Payment Receipt - Modern",
+    "summary": "Modern receipt for customer payments with transfer and e-check details",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Reporting",
+    "depends": ["account", "payment_receipt_cheques"],
+    "data": ["reports/report_payment_receipt_modern.xml"],
+    "license": "LGPL-3",
+    "installable": true,
+    "application": false,
+    "author": "Esteban + ChatGPT"
+}
