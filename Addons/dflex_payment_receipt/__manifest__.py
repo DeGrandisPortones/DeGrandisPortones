f361faf755b5a9d8a760a726d3581f95b4609c16
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-04 19:07:48 -0300

    yes

--- Addons/dflex_payment_receipt/__manifest__.py
+++ Addons/dflex_payment_receipt/__manifest__.py
@@ -1,8 +1,8 @@
 
 {
     'name': 'DFlex Payment Receipt (Modern)',
-    'summary': 'Recibo de pago con detalle de cheques (ADHOC compat + sin getattr)',
-    'version': '18.0.1.0.8',
+    'summary': 'Recibo de pago con detalle de cheques (Latam/ADHOC: l10n_latam_new_check_ids & l10n_latam_move_check_ids)',
+    'version': '18.0.1.0.9',
     'category': 'Accounting/Reporting',
     'depends': ['account', 'web'],
     'data': ['reports/report_payment_receipt.xml'],
