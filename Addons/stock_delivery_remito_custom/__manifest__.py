a69800386a1ac3068d7e66425663bb7313c8146a
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-04 14:21:12 -0300

    with stock

---
+++ Addons/stock_delivery_remito_custom/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Stock Delivery Remito Custom",
+    "summary": "Custom 'Remito' (Delivery Slip) layout similar to the provided design",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Reporting",
+    "depends": ["stock", "web"],
+    "data": ["reports/report_delivery_remito.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "author": "Esteban + ChatGPT"
+}
