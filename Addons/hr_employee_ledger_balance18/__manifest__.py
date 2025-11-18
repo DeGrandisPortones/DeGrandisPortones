c9489bf016cbbccb8c77e6d9e6bfd9dacfd24519
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-18 09:56:06 -0300

    balanc

--- Addons/hr_employee_ledger_balance18/__manifest__.py
+++ Addons/hr_employee_ledger_balance18/__manifest__.py
@@ -1,14 +1,11 @@
 {
-    "name": "HR Employee Ledger Balance (CC View)",
+    "name": "HR Employee Ledger Balance (A/B)",
     "version": "18.0.1.0.0",
     "author": "Dflex Argentina SAS",
     "category": "Human Resources",
     "depends": ["hr", "hr_employee_ledger_simple18"],
     "data": [
-        "security/ir.model.access.csv",
-        "views/balance_views.xml",
-        "views/menu.xml",
-        "views/employee_button_inherit.xml"
+        "views/employee_balance_views.xml"
     ],
     "installable": True,
     "application": False,
