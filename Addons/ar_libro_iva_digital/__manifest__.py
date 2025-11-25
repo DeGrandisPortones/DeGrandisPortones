8af456d6e21ba0d55dc7545921f79f1ce6b436c7
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-11-25 12:25:01 -0300

    push

--- Addons/ar_libro_iva_digital/__manifest__.py
+++ Addons/ar_libro_iva_digital/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "AR - Libro IVA Digital (Compras/Ventas)",
     "summary": "Genera TXT AFIP: CBTE y ALICUOTAS para Compras/Ventas",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "license": "AGPL-3",
     "category": "Accounting/Localizations",
     "author": "DeGrandisPortones",
