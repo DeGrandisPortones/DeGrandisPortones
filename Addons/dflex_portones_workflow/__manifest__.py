4ecefea4d566bfcc5d222898dd3590c397c796b4
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-01 11:35:01 -0300

    go

--- Addons/dflex_portones_workflow/__manifest__.py
+++ Addons/dflex_portones_workflow/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "1.0.0",
     "author": "Esteban + ChatGPT",
     "license": "LGPL-3",
-    "depends": ["base", "sale", "x_portones"],  # x_portones = módulo estándar donde está el modelo x_dflex.porton
+    "depends": ["base", "sale", "x_dflex.porton"],  # x_portones = módulo estándar donde está el modelo x_dflex.porton
     "data": [
         "security/dflex_portones_security.xml",
         "views/dflex_porton_workflow_views.xml",
