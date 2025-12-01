c24c970752da7a14cc6bed1250e16c3456754b9e
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-01 10:41:17 -0300

    Testing

---
+++ Addons/dflex_portones_workflow/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "dflex_portones_workflow",
+    "version": "18.0.1.0.0",
+    "summary": "Workflow de portones: acopio, medición, pre-producción y aprobaciones",
+    "category": "Sales/CRM",
+    "author": "Esteban + ChatGPT",
+    "license": "LGPL-3",
+    "depends": ["sale_management", "mrp"],
+    "data": [
+        "security/dflex_portones_security.xml",
+        "security/ir.model.access.csv",
+        "views/dflex_porton_workflow_views.xml"
+    ],
+    "installable": true,
+    "application": false
+}
---
+++ Addons/dflex_portones_workflow/models/__init__.py
@@ -0,0 +1,1 @@
+from . import dflex_porton_workflow
