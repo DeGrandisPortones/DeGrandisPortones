01a0417da97c941681541d2f2e4f4e6481d33684
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-01 11:16:12 -0300

    opl

--- Addons/dflex_portones_workflow/__manifest__.py
+++ Addons/dflex_portones_workflow/__manifest__.py
@@ -1,19 +1,14 @@
 {
-    "name": "DFLEX Portones - Workflow",
-    "version": "16.0.1.0.0",
-    "author": "ChatGPT",
+    "name": "DFLEX Portones Workflow",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "DFLEX Argentina",
     "category": "Sales",
-    "license": "OPL-1",
-    "depends": [
-        "sale",
-        "mrp"
-    ],
+    "summary": "Campos básicos de workflow para portones",
     "data": [
-        "security/dflex_portones_security.xml",
-        "security/ir.model.access.csv",
         "views/dflex_porton_workflow_views.xml"
     ],
     "installable": True,
     "application": False,
-    "auto_install": False
+    "license": "OPL-1"
 }
