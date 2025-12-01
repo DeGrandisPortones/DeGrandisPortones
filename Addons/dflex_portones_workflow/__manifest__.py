e8eb608a27eda133263296bd95d84ee3f2fc0160
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-01 11:39:51 -0300

    rata blanca

--- Addons/dflex_portones_workflow/__manifest__.py
+++ Addons/dflex_portones_workflow/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 {
     "name": "DFLEX Portones - Workflow",
-    "summary": "Estados y aprobaciones del portón (Acopio / Producción)",
-    "version": "1.0.0",
-    "author": "Esteban + ChatGPT",
-    "license": "LGPL-3",
-    "depends": ["base", "sale", "x_dflex.porton"],  # x_portones = módulo estándar donde está el modelo x_dflex.porton
+    "version": "1.0",
+    "author": "DFLEX ARGENTINA S.A.S.",
+    "category": "Sales",
+    "depends": ["base", "sale"],
     "data": [
         "security/dflex_portones_security.xml",
+        "security/ir.model.access.csv",
         "views/dflex_porton_workflow_views.xml",
-        "views/dflex_porton_menu_views.xml",
+        "views/dflex_porton_menus.xml"
     ],
     "installable": True,
     "application": False,
+    "license": "LGPL-3"
 }
