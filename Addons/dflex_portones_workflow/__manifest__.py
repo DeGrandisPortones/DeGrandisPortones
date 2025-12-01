24396fa0c3e0fc36004525187ec553e22ac903a9
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-01 12:39:45 -0300

    simple

--- Addons/dflex_portones_workflow/__manifest__.py
+++ Addons/dflex_portones_workflow/__manifest__.py
@@ -1,18 +1,15 @@
 {
-    'name': 'DFLEX Portones - Workflow portones',
-    'version': '16.0.1.0.0',
-    'summary': 'Estados y aprobaciones del portón (Acopio / Medición / Pre-producción / Producción)',
+    'name': 'DFLEX Portones Workflow',
+    'version': '18.0.1.0.0',
+    'summary': 'Workflow de estados y aprobaciones para portones DFLEX',
+    'category': 'Sales',
     'author': 'Esteban + ChatGPT',
-    'category': 'Sales/CRM',
+    'website': '',
     'license': 'LGPL-3',
-    'depends': [
-        'base',
-    ],
+    'depends': ['base'],
     'data': [
-        'security/dflex_portones_security.xml',
         'views/dflex_porton_workflow_views.xml',
     ],
     'installable': True,
     'application': False,
-    'auto_install': False,
 }
