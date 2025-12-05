ba720e276ed2ed039205af3d6a7a456cf232166a
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-05 14:43:06 -0300

    a

--- Addons/dflex_porton_sync/__manifest__.py
+++ Addons/dflex_porton_sync/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dflex Porton Sync",
-    "version": "18.0.6.0.0",
+    "version": "18.0.7.0.0",
     "depends": ["sale"],
     "author": "Dflex Argentina SAS",
     "category": "Sales",
@@ -9,12 +9,6 @@
         "views/porton_views.xml",
         "views/sql_quotation_wizard_views.xml"
     ],
-    "assets": {
-        "web.assets_backend": [
-            "dflex_porton_sync/static/src/js/porton_formula_widget.js",
-            "dflex_porton_sync/static/src/xml/porton_formula_widget.xml"
-        ]
-    },
     "installable": True,
     "application": False
 }
