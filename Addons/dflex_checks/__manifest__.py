5c69ec4834e9185a3e4f42a77ca22ca681cff6fd
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2025-12-16 09:40:37 -0300

    Parseerror

--- Addons/dflex_checks/__manifest__.py
+++ Addons/dflex_checks/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "DFlex - Cheques Propios",
     "summary": "Gestión de chequeras y cheques propios (físicos y eCheq)",
     "description": "Gestión de chequeras y cheques propios.",
-    "version": "18.0.1.0.5",
+    "version": "18.0.1.0.6",
     "author": "DFLEX Argentina SAS",
     "website": "https://dflex.com.ar",
     "category": "Accounting/Payments",
