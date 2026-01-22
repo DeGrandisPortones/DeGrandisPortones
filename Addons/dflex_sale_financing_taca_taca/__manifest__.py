75ed9d7bd7b966ddb521ec80179238bb9c4d0dfc
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-01-22 08:04:48 -0300

    Tatunium

--- Addons/dflex_sale_financing_taca_taca/__manifest__.py
+++ Addons/dflex_sale_financing_taca_taca/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Dflex - Financiamiento Taca Taca (Recargo por Cuotas)",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Sales/Sales",
     "summary": "Aplica recargo porcentual por cuotas según plan/tarjeta, recalculando el precio unitario por línea.",
     "author": "Dflex Argentina SAS",
