e854b4ad94b71064c7213310ea32321bcde5cbe8
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-02-20 11:59:43 -0300

    pumba

---
+++ Addons/l10n_latam_check_direct_deposit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "LATAM Third Party Checks - Direct Bank Deposit",
+    "summary": "Depositar cheques de terceros directamente en el banco (sin cuenta puente)",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Localizations",
+    "license": "LGPL-3",
+    "author": "Dflex Argentina SAS",
+    "depends": [
+        "l10n_latam_check_ux",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
