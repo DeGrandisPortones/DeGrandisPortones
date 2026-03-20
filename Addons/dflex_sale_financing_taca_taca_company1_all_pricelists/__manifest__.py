4108a8ec6dcedb28ab939e01ee3dc0b4355215af
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-03-20 09:21:27 -0300

    taca taca

---
+++ Addons/dflex_sale_financing_taca_taca_company1_all_pricelists/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Dflex - Taca Taca empresa 1 todas las listas",
+    "version": "18.0.1.0.0",
+    "category": "Sales/Sales",
+    "summary": "Habilita Taca Taca en DFLEX ARGENTINA SAS para cualquier lista de precios.",
+    "author": "Dflex Argentina SAS",
+    "license": "LGPL-3",
+    "depends": ["dflex_sale_financing_taca_taca"],
+    "data": [],
+    "application": False,
+    "installable": True,
+}
