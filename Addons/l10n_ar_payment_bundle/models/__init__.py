20b7593280e12e651b53f9a667656a6ac5164903
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-04-24 14:50:31 -0300

    refix

--- Addons/l10n_ar_payment_bundle/models/__init__.py
+++ Addons/l10n_ar_payment_bundle/models/__init__.py
@@ -1,6 +1,6 @@
 from . import res_company
 from . import account_chart_template
 from . import account_payment
-from . import account_payment_available_journal_fix
 from . import account_payment_method
 from . import account_journal
+from . import account_payment_bundle_visibility
--- Addons/l10n_ar_payment_bundle/models/res_company.py
+++ Addons/l10n_ar_payment_bundle/models/res_company.py
@@ -4,27 +4,21 @@ from odoo import models, tools
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    @tools.ormcache("payment_type")
+    @tools.ormcache("self.id", "payment_type")
     def _get_bundle_journal(self, payment_type: str) -> int:
-        if payment_type == "inbound":
-            return (
-                self.env["account.journal"]
-                .search(
-                    [
-                        ("inbound_payment_method_line_ids.payment_method_id.code", "=", "payment_bundle"),
-                        ("company_id", "=", self.id),
-                    ]
-                )
-                .id
+        method_field = (
+            "inbound_payment_method_line_ids"
+            if payment_type == "inbound"
+            else "outbound_payment_method_line_ids"
+        )
+        return (
+            self.env["account.journal"]
+            .search(
+                [
+                    (f"{method_field}.payment_method_id.code", "=", "payment_bundle"),
+                    ("company_id", "=", self.id),
+                ],
+                limit=1,
             )
-        else:
-            return (
-                self.env["account.journal"]
-                .search(
-                    [
-                        ("inbound_payment_method_line_ids.payment_method_id.code", "=", "payment_bundle"),
-                        ("company_id", "=", self.id),
-                    ]
-                )
-                .id
-            )
+            .id
+        )
