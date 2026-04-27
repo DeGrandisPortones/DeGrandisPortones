9a56dec51da883ddf83a26d08291a11e2fa5e745
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-04-27 14:27:15 -0300

    fix hidder

--- Addons/dflex_checks/models/__init__.py
+++ Addons/dflex_checks/models/__init__.py
@@ -3,3 +3,4 @@ from . import check
 from . import payment_check_line
 from . import l10n_latam_check
 from . import account_payment
+from . import account_payment_view_adjust
---
+++ Addons/dflex_checks/models/account_payment_view_adjust.py
@@ -0,0 +1,36 @@
+from lxml import etree
+
+from odoo import models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    def get_view(self, view_id=None, view_type="form", **options):
+        res = super().get_view(view_id=view_id, view_type=view_type, **options)
+        if view_type != "form" or not res.get("arch"):
+            return res
+
+        try:
+            arch = etree.fromstring(res["arch"])
+        except Exception:
+            return res
+
+        changed = False
+        check_grid_fields = arch.xpath("//field[@name='l10n_latam_new_check_ids']")
+
+        if check_grid_fields:
+            # Tipo y Orden son datos de cheques de terceros. En cheques propios se cargan
+            # automáticamente o no aplican, por eso se ocultan cuando el método es own_checks.
+            for field_name in ("x_studio_tipo_cheque", "ux_order_type"):
+                for node in arch.xpath(
+                    "//field[@name='l10n_latam_new_check_ids']//field[@name='%s']" % field_name
+                ):
+                    node.set("column_invisible", "parent.payment_method_code == 'own_checks'")
+                    node.set("invisible", "parent.payment_method_code == 'own_checks'")
+                    node.set("required", "parent.payment_method_code != 'own_checks'")
+                    changed = True
+
+        if changed:
+            res["arch"] = etree.tostring(arch, encoding="unicode")
+        return res
