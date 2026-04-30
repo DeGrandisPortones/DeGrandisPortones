17ccd41a17468cff21ef7920f1755cb4ab6b065b
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-04-30 12:09:56 -0300

    ddaa

--- Addons/l10n_latam_check_ux/wizards/__init__.py
+++ Addons/l10n_latam_check_ux/wizards/__init__.py
@@ -1,1 +1,8 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
 from . import account_check_action_wizard
+from . import l10n_latam_payment_mass_transfer
+from . import checks_to_date_report
+from . import l10n_latam_check_history
