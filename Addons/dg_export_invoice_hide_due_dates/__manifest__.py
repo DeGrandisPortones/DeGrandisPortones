9c0f96e5e99d3ef1bd4703315a4f67a643c03a09
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-06-02 14:37:34 -0300

    asd

---
+++ Addons/dg_export_invoice_hide_due_dates/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DG Factura E sin fechas de vencimiento",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Localizations",
+    "summary": "Oculta fechas de vencimiento comerciales y de CAE en el PDF de comprobantes E.",
+    "author": "Dflex Argentina SAS",
+    "license": "LGPL-3",
+    "depends": ["account", "l10n_ar", "l10n_ar_ux"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
