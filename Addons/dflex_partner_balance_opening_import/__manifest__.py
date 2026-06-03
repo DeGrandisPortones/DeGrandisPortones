96ff1a5cf4c3ee184bb8218b86dff157e419ba48
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-06-03 12:20:24 -0300

    Importador de saldos de clientes

---
+++ Addons/dflex_partner_balance_opening_import/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DFlex - Importar saldos iniciales de contactos",
+    "summary": "Importa saldos iniciales de clientes separados por FCA e Interno contra Capital integrado",
+    "version": "18.0.1.0.0",
+    "author": "DFLEX Argentina SAS",
+    "website": "https://dflex.com.ar",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/partner_balance_opening_import_wizard_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
