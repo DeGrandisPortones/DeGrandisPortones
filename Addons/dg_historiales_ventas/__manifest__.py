ba5a83a8d5fff0d845387316840fad53617348dc
Esteban Javier Scalerandi <estebanscalerandi.dev@gmail.com>
2026-06-17 07:01:27 -0300

    feat: add dg_historiales_ventas module with external app shortcuts

---
+++ Addons/dg_historiales_ventas/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Historiales de Ventas",
+    "summary": "Accesos directos al Presupuestador y al Informe de Ventas desde el menú de Odoo",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "author": "DeGrandis Portones",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
